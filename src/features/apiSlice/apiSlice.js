11a37a6b32fb7c221d8e8634a58c393956e80382
Piyashhasan <piyashhasan38@gmail.com>
2024-09-26 01:29:56 +0600

    feat:Happy coading

--- src/features/apiSlice/apiSlice.js
+++ src/features/apiSlice/apiSlice.js
@@ -1,10 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const baseUrl = import.meta.env.VITE_SERVER_URL;
+
 // --- root app api query ---
 export const apiSlice = createApi({
   reducerPath: "tasksApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://task-track-server.vercel.app/task",
+    baseUrl: `${baseUrl}/task`,
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("token");
       if (token) {
